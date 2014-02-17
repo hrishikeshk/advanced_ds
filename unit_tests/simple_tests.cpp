3025cb124e554bd3e5c2da9526cdd25a93abbc68
Hrishikesh VK <kv_hrishikesh@yahoo.com>
2014-02-17 18:34:37 +0530

    Working simple unit tests for RBNode, RBTree

--- unit_tests/simple_tests.cpp
+++ unit_tests/simple_tests.cpp
@@ -1,34 +1,35 @@
-//#define BOOST_TEST_MODULE simple_tests
-#define BOOST_TEST_MODULE example
+#define BOOST_TEST_MODULE test_suite1
 #include <boost/test/included/unit_test.hpp>
 
 #include "RBNode.h"
 #include "RBTree.h"
 #include "Test_Key.h"
+#include <iostream>
 
 using namespace boost::unit_test;
 
 void simple_ctor_RBNode(){
 	RBNode rbn;
-	BOOST_CHECK(rbn.m_color == BLACK);
+	BOOST_REQUIRE(rbn.m_color == BLACK);
 }
 
 void simple_ctor_RBTree(){
 	RBTree rbt;
 	Test_Key tk;
 	UInt32 ox = 0;
-	BOOST_CHECK(rbt.find_element(tk, ox) == false);
+	BOOST_REQUIRE(rbt.find_element(tk, ox) == false);
 }
 
-BOOST_AUTO_TEST_CASE( testA ){
-	BOOST_CHECK(true);
-}
-/*
-BOOST_AUTO_TEST_CASE( simple_ctor_RBNode ){
-	BOOST_CHECK(true);
+BOOST_AUTO_TEST_SUITE ( test_suite1 )
+
+BOOST_AUTO_TEST_CASE( ctor_RBNode ){
+	simple_ctor_RBNode();
+
 }
 
-BOOST_AUTO_TEST_CASE( simple_ctor_RBTree ){
-	BOOST_CHECK(true);
+BOOST_AUTO_TEST_CASE( ctor_RBTree ){
+	simple_ctor_RBTree();
 }
-*/
+
+BOOST_AUTO_TEST_SUITE_END()
+
