92254da73cf78023f88aef920c418721d9728f76
Hrishikesh VK <kv_hrishikesh@yahoo.com>
2014-08-05 00:04:02 +0530

    A new BitMap structure, early stages

---
+++ bloom_filter/BitMap.cpp
@@ -0,0 +1,27 @@
+#include "TypeDefs.h"
+#include "Consts.h"
+
+#include "Common_Funcs.h"
+
+#include "BitMap.h"
+
+BitMap::BitMap(UInt32 size_in_bytes): m_bytes(size_in_bytes), m_buffer(new UInt8[size_in_bytes]){
+
+}
+
+BitMap::~BitMap(){
+	delete[] m_buffer;
+}
+
+Bool BitMap::mark(UInt32 offset){
+
+}
+
+Bool BitMap::is_marked(UInt32 offset) const{
+
+}
+
+Bool BitMap::unmark(UInt32 offset){
+
+}
+
---
+++ bloom_filter/BitMap.h
@@ -0,0 +1,31 @@
+#ifndef _BLOOM_FILTER_
+#define _BLOOM_FILTER_
+
+#include "TypeDefs.h"
+#include "Consts.h"
+
+#include "Dependent_Includes.h"
+
+class BitMap{
+
+	public:
+	
+	BitMap(UInt32 size_in_bytes);
+
+	~BitMap();
+
+	Bool mark(UInt32 offset); // This offset will be marked, it should be within range of the map size
+
+	Bool is_marked(UInt32 offset) const; // Simple read-only check 
+
+	Bool unmark(UInt32 offset); // No issues if the offset is already unmarked
+
+	private:
+
+		UInt32 m_bytes;
+
+		UInt8*	m_buffer;
+};
+
+#endif
+
